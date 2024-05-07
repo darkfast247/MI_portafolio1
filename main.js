e4cb4cfa03ea8167a74aec3c3e18231a151352d2
SantiagoLoaiza247 <128566879+darkfast247@users.noreply.github.com>
2024-05-07 12:50:10 -0500

    commit actualizacion de algunos elementos

---
+++ main.js
@@ -0,0 +1,14 @@
+formulario = document.querySelector('.formulario');
+nombre = document.querySelector("#nombre");
+email = document.querySelector("#email");
+mensaje = document.querySelector("#mensaje");
+btn = document.querySelector("#btnEnviar");
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (nombre.value === "" || email.value === "" || mensaje.value === "") {
+    alert("Todos los campos deben estar diligenciados, gracias");
+    return;
+  }
+  formulario.submit();
+});
